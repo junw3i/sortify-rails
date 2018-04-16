6962bbf13346fe4ee8b84e69bf45b15265986e75
junw3i <cj@Juns-MacBook.local>
2018-04-16 22:57:23 +0800

    sloppy code

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,4 +13,20 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @genres = Genre.all
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @song.save
+
+    redirect_to @song
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:name, :genre_ids => [])
+  end
 end
