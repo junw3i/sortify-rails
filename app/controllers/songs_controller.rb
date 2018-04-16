30b7ea4d91f2117ad972ae1392aee22987354093
junw3i <cj@Juns-MacBook.local>
2018-04-16 21:16:34 +0800

    part 1

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,12 @@
+class SongsController < ApplicationController
+  def index
+    @genre_id = params[:genre_id]
+    
+    if @genre_id.nil?
+      @songs = Song.all
+    else
+      @songs = Song.where(genre_id: @genre_id)
+    end
+
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+classical = Genre.new
+classical.name = "Classical"
+classical.description = "Songs for sleeping"
+classical.save
+
+rock = Genre.new
+rock.name = "Rock"
+rock.description = "Songs for Engergy"
+rock.save
+
+bridge = Song.new
+bridge.name = "Under the bridge"
+bridge.genre = rock
+bridge.save
+
+tv = Song.new
+tv.name = "Television"
+tv.genre = rock
+tv.save
+
+sym = Song.new
+sym.name = "Symfony"
+sym.genre = classical
+sym.save
