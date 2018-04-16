64d9ea2e8898e9ad0fb3596c76f2fd580b236ecb
junw3i <cj@Juns-MacBook.local>
2018-04-16 22:34:19 +0800

    part 2

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,16 @@
 class SongsController < ApplicationController
   def index
     @genre_id = params[:genre_id]
-    
+
     if @genre_id.nil?
       @songs = Song.all
     else
       @songs = Song.where(genre_id: @genre_id)
     end
 
+  end
+
+  def show
+    @song = Song.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-classical = Genre.new
-classical.name = "Classical"
-classical.description = "Songs for sleeping"
-classical.save
+# classical = Genre.new
+# classical.name = "Classical"
+# classical.description = "Songs for sleeping"
+# classical.save
+#
+# rock = Genre.new
+# rock.name = "Rock"
+# rock.description = "Songs for Engergy"
+# rock.save
+#
+# bridge = Song.new
+# bridge.name = "Under the bridge"
+# bridge.genre = rock
+# bridge.save
+#
+# tv = Song.new
+# tv.name = "Television"
+# tv.genre = rock
+# tv.save
+#
+# sym = Song.new
+# sym.name = "Symfony"
+# sym.genre = classical
+# sym.save
 
 rock = Genre.new
-rock.name = "Rock"
-rock.description = "Songs for Engergy"
+rock.name = "90s"
+rock.description = "Songs for the 90s"
 rock.save
 
-bridge = Song.new
-bridge.name = "Under the bridge"
-bridge.genre = rock
-bridge.save
+disco = Genre.new
+disco.name = "Disco"
+disco.description = "Songs for the wild"
+disco.save
 
-tv = Song.new
-tv.name = "Television"
-tv.genre = rock
-tv.save
+frog = Song.new
+frog.name = "Froggy Song"
+frog.save
 
-sym = Song.new
-sym.name = "Symfony"
-sym.genre = classical
-sym.save
+frog.genres << rock
